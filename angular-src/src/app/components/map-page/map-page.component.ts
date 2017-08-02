1bf1f969b2fb0c88e3baee875360fd0358efbd77
sadeajayi <folasadeajayi@hotmail.com>
2017-08-02 06:02:26 -0400

    modified map page

--- angular-src/src/app/components/map-page/map-page.component.ts
+++ angular-src/src/app/components/map-page/map-page.component.ts
@@ -1,15 +1,5 @@
 import { Component, NgModule } from '@angular/core';
-<<<<<<< HEAD
 import {MarkerService} from "../../services/marker.service";
-=======
-import { ElementRef, NgZone, OnInit, ViewChild } from '@angular/core';
-
-
-import { BrowserModule } from '@angular/platform-browser';
-import { FormControl } from "@angular/forms";
-import { ReactiveFormsModule } from "@angular/forms";
-import { FormsModule } from "@angular/forms";
->>>>>>> 6562fe63a691a1a71d43d51cf4e49d5b0ab37965
 
 declare var google: any;
 
@@ -18,7 +8,6 @@ declare var google: any;
   templateUrl: './map-page.component.html',
   styleUrls: ['./map-page.component.css'],
 })
-<<<<<<< HEAD
 export class MapPageComponent {
 
   lat: number =  6.5244;
@@ -94,60 +83,6 @@ export class MapPageComponent {
 
   ngOnInit() {
   
-=======
-export class MapPageComponent implements OnInit{
-
-  title: string = 'My first AGM project';
-  lat: number =  6.5244;
-  long: number = 3.3792;
-  /*searchControl: FormControl;
-  */
-  zoom: number = 13;
-
-  clickedMarker(label: string, index: number) {
-    console.log(`clicked the marker: ${label || index}` )
-  }
-
-
-  mapClicked($event:any) {
-      console.log('YOU CAN CLICK');
-      var newMarker = {
-        lat: $event.coords.lat,
-        long: $event.coords.long,
-        draggable: false
-      }
-      this.markers.push(newMarker); 
-  }
-
-
-markerDragEnd(m: marker, $event: MouseEvent) {
-    console.log('dragEnd', m, $event);
-  }
-
-  markers: marker[] = [
-	  {
-		  lat:6.4338,
-		  long: 3.4220,
-		  draggable: true
-	  },
-	  {
-		  lat: 6.4561,
-		  long: 3.4306,
-		  draggable: false
-	  }
-  ]
-/*
-@ViewChild("search")
-  public searchElementRef: ElementRef;
-  */
-  constructor(
-
-  ) {}
-
-  ngOnInit() {
-    
-    //set google maps defaults
->>>>>>> 6562fe63a691a1a71d43d51cf4e49d5b0ab37965
     /*
     var map = new google.maps.Map(document.getElementById('map'), {
           center: new google.maps.LatLng(6.5244, 3.3792),
