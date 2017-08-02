67ea3f5d6e765fe99df0443d2b538770ceca5742
sadeajayi <folasadeajayi@hotmail.com>
2017-08-02 18:17:05 -0400

    reinstalled node

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ var db = mongoose.connection;
 
 //handle mongo error
 db.on('error', console.error.bind(console, 'connection error:'));
-db.once('open', function () {
+db.once('openUri', function () {
   // we're connected!
 });
 
