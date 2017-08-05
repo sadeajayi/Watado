b778b976396931a8c2b577a18ba45c2b3fc5b6b5
Sade <fajayi39@gmail.com>
2017-08-05 18:48:02 -0400

    removed env

--- app.js
+++ app.js
@@ -103,9 +103,7 @@ app.listen(3000, function () {
   console.log('Watado app listening on port 3000');
 });
 */
-if (process.env.NODE_ENV !== 'production') {
-require('dotenv').config()
-}
+
 
 app.listen(port, () => {
   console.log('Server started on port ' + port);
