e96c3aeacd85b22d4778f6b3cbd5c37103e6cca2
Sade <fajayi39@gmail.com>
2017-08-05 07:18:12 -0400

    added URI

--- config/database.js
+++ config/database.js
@@ -1,6 +1,6 @@
 module.exports = {
   //database: 'mongodb://localhost:27017/watado',  
   
-  database:'//watado:watado@ds155961.mlab.com:55961/watado',
+  database:'mongodb://heroku_362jnj2q:uiqngj64r6pjvf41vmnquo32go@ds135983.mlab.com:35983/heroku_362jnj2q',
   secret: 'yoursecret'
 }
